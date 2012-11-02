63386d382d92bb046a591d7494477edf985e14da
gqqnb2005@gmail.com <gqqnb2005@gmail.com@a9e5eacc-42ef-996c-78fa-4dedd5e0f03a>
2012-11-02 08:02:07 +0000

    增加了CaptureViewer。

---
+++ xiwan/trunk/CaptureViewer.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gqqnbig.TrafficVolumeCalculator
+{
+    /// <summary>
+    /// CaptureViewer.xaml 的交互逻辑
+    /// </summary>
+    public partial class CaptureViewer : UserControl
+    {
+        public CaptureViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var adornerLayer = AdornerLayer.GetAdornerLayer(imageBox);
+            Car carGroup;
+            if (e.RemovedItems.Count > 0)
+            {
+                carGroup = (Car)e.RemovedItems[0];
+                var adorners = adornerLayer.GetAdorners(imageBox);
+                if (adorners != null)
+                {
+                    for (int i = 0; i < adorners.Length; i++)
+                    {
+                        OutBoxAdorner oba = adorners[i] as OutBoxAdorner;
+                        if (oba != null && oba.Rectangle.Equals(carGroup.CarRectangle) && oba.Pen.Brush == Brushes.Orange)
+                        {
+                            adornerLayer.Remove(adorners[i]);
+                            break;
+                        }
+
+                    }
+                }
+            }
+
+            if (e.AddedItems.Count != 1)
+                return;
+            carGroup = (Car)e.AddedItems[0];
+            OutBoxAdorner outBoxAdorner = new OutBoxAdorner(imageBox);
+            outBoxAdorner.Pen = new Pen(Brushes.Orange, 2);
+            outBoxAdorner.Rectangle = carGroup.CarRectangle;
+            adornerLayer.Add(outBoxAdorner);
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            Car car = (Car)checkBox.DataContext;
+            var adornerLayer = AdornerLayer.GetAdornerLayer(imageBox);
+
+            //加上Adorner
+            OutBoxAdorner outBoxAdorner = new OutBoxAdorner(imageBox);
+            outBoxAdorner.Pen = new Pen(Brushes.LightGreen, 2);
+            outBoxAdorner.Rectangle = car.CarRectangle;
+            adornerLayer.Add(outBoxAdorner);
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            Car carGroup = (Car)checkBox.DataContext;
+            var adornerLayer = AdornerLayer.GetAdornerLayer(imageBox);
+            var adorners = adornerLayer.GetAdorners(imageBox);
+            if (adorners != null)
+            {
+                for (int i = 0; i < adorners.Length; i++)
+                {
+                    OutBoxAdorner oba = adorners[i] as OutBoxAdorner;
+                    if (oba != null && oba.Rectangle.Equals(carGroup.CarRectangle) && oba.Pen.Brush == Brushes.LightGreen)
+                    {
+                        adornerLayer.Remove(adorners[i]);
+                        //break;
+                    }
+
+                }
+            }
+        }
+
+        private void saveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage context = (BitmapImage)((MenuItem)sender).DataContext;
+
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.DefaultExt = ".bmp";
+            if (dialog.ShowDialog().GetValueOrDefault(false))
+            {
+                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+
+                Stream stream = dialog.OpenFile();
+                encoder.Frames.Add(BitmapFrame.Create(context));
+                encoder.Save(stream);
+                stream.Close();
+            }
+        }
+    }
+}
--- xiwan/trunk/MainWindow.xaml.cs
+++ xiwan/trunk/MainWindow.xaml.cs
@@ -3,8 +3,10 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using System.IO;
 
 namespace Gqqnbig.TrafficVolumeCalculator
 {
@@ -15,40 +17,10 @@ namespace Gqqnbig.TrafficVolumeCalculator
     {
         public const string filePathPattern = @"H:\文件\毕业设计\西湾大桥氹仔端\图片\{0}.jpg";
 
-        readonly ListView listView1, listView2;
-
 
         public MainWindow()
         {
             InitializeComponent();
-
-            listView1 = new ListView();
-            Grid.SetColumn(listView1, 1);
-            Grid.SetRow(listView1, 0);
-            ScrollViewer.SetHorizontalScrollBarVisibility(listView1, ScrollBarVisibility.Auto);
-            listView1.SelectionChanged += contourListBox_SelectionChanged;
-            GridView gridView = new GridView();
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("checkBoxTemplate") });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("picTemplate"), Header = "图片" });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("histRTemplate"), Header = "R" });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("histGTemplate"), Header = "G" });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("histBTemplate"), Header = "B" });
-            listView1.View = gridView;
-            grid.Children.Add(listView1);
-
-            listView2 = new ListView();
-            Grid.SetColumn(listView2, 1);
-            Grid.SetRow(listView2, 1);
-            ScrollViewer.SetHorizontalScrollBarVisibility(listView2, ScrollBarVisibility.Auto);
-            listView2.SelectionChanged += contourListBox_SelectionChanged;
-            gridView = new GridView();
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("checkBoxTemplate") });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("picTemplate"), Header = "图片" });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("histRTemplate"), Header = "R" });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("histGTemplate"), Header = "G" });
-            gridView.Columns.Add(new GridViewColumn { CellTemplate = (DataTemplate)FindResource("histBTemplate"), Header = "B" });
-            listView2.View = gridView;
-            grid.Children.Add(listView2);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -59,16 +31,16 @@ namespace Gqqnbig.TrafficVolumeCalculator
                 Lane lane = new Lane();
                 Image<Gray, byte> finalImage;
                 var cars1 = lane.FindCars(frame1, out finalImage);
-                image1.Source = lane.GetFocusArea(frame1).ToBitmap().ToBitmapImage();
-                listView1.ItemsSource = cars1;
-                totalCarNumberTextRun.Text = cars1.Length.ToString();
+                captureViewer1.imageBox.Source = lane.GetFocusArea(frame1).ToBitmap().ToBitmapImage();
+                captureViewer1.listView.ItemsSource = cars1;
+                captureViewer1.totalCarNumberTextRun.Text = cars1.Length.ToString();
 
 
                 Image<Bgr, byte> frame2 = new Image<Bgr, byte>(@"H:\文件\毕业设计\西湾大桥氹仔端\图片\1.jpg");
                 var cars2 = lane.FindCars(frame2, out finalImage);
-                image2.Source = lane.GetFocusArea(frame2).ToBitmap().ToBitmapImage();// finalImage.ToBitmap().ToBitmapImage();
-                listView2.ItemsSource = cars2;
-                totalCarNumberTextRun2.Text = cars2.Length.ToString();
+                captureViewer2.imageBox.Source = lane.GetFocusArea(frame2).ToBitmap().ToBitmapImage();// finalImage.ToBitmap().ToBitmapImage();
+                captureViewer2.listView.ItemsSource = cars2;
+                captureViewer2.totalCarNumberTextRun.Text = cars2.Length.ToString();
             }
             catch (Exception ex)
             {
@@ -77,140 +49,9 @@ namespace Gqqnbig.TrafficVolumeCalculator
         }
 
 
-
-        private void contourListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var adornerLayer = AdornerLayer.GetAdornerLayer(image1);
-            Car carGroup;
-            if (e.RemovedItems.Count > 0)
-            {
-                carGroup = (Car)e.RemovedItems[0];
-                var adorners = adornerLayer.GetAdorners(image1);
-                if (adorners != null)
-                {
-                    for (int i = 0; i < adorners.Length; i++)
-                    {
-                        OutBoxAdorner oba = adorners[i] as OutBoxAdorner;
-                        if (oba != null && oba.Rectangle.Equals(carGroup.CarRectangle) && oba.Pen.Brush == Brushes.Orange)
-                        {
-                            adornerLayer.Remove(adorners[i]);
-                            break;
-                        }
-
-                    }
-                }
-            }
-
-            carGroup = (Car)e.AddedItems[0];
-            OutBoxAdorner outBoxAdorner = new OutBoxAdorner(image1);
-            outBoxAdorner.Pen = new Pen(Brushes.Orange, 2);
-            outBoxAdorner.Rectangle = carGroup.CarRectangle;
-            adornerLayer.Add(outBoxAdorner);
-
-        }
-
-        //private void contourListBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    var adornerLayer = AdornerLayer.GetAdornerLayer(image2);
-        //    Car carGroup;
-        //    if (e.RemovedItems.Count > 0)
-        //    {
-        //        carGroup = (Car)e.RemovedItems[0];
-        //        var adorners = adornerLayer.GetAdorners(image2);
-        //        if (adorners != null)
-        //        {
-        //            for (int i = 0; i < adorners.Length; i++)
-        //            {
-        //                OutBoxAdorner oba = adorners[i] as OutBoxAdorner;
-        //                if (oba != null && oba.Rectangle.Equals(carGroup.CarRectangle) && oba.Pen.Brush == Brushes.Orange)
-        //                {
-        //                    adornerLayer.Remove(adorners[i]);
-        //                    break;
-        //                }
-
-        //            }
-        //        }
-        //    }
-
-        //    carGroup = (Car)e.AddedItems[0];
-        //    OutBoxAdorner outBoxAdorner = new OutBoxAdorner(image2);
-        //    outBoxAdorner.Pen = new Pen(Brushes.Orange, 2);
-        //    outBoxAdorner.Rectangle = carGroup.CarRectangle;
-        //    adornerLayer.Add(outBoxAdorner);
-
-        //}
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            CheckBox checkBox = (CheckBox)sender;
-            Car car = (Car)checkBox.DataContext;
-            var adornerLayer = AdornerLayer.GetAdornerLayer(image1);
-
-            //加上Adorner
-            OutBoxAdorner outBoxAdorner = new OutBoxAdorner(image1);
-            outBoxAdorner.Pen = new Pen(Brushes.LightGreen, 2);
-            outBoxAdorner.Rectangle = car.CarRectangle;
-            adornerLayer.Add(outBoxAdorner);
-        }
-
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            CheckBox checkBox = (CheckBox)sender;
-            Car carGroup = (Car)checkBox.DataContext;
-            var adornerLayer = AdornerLayer.GetAdornerLayer(image1);
-            var adorners = adornerLayer.GetAdorners(image1);
-            if (adorners != null)
-            {
-                for (int i = 0; i < adorners.Length; i++)
-                {
-                    OutBoxAdorner oba = adorners[i] as OutBoxAdorner;
-                    if (oba != null && oba.Rectangle.Equals(carGroup.CarRectangle) && oba.Pen.Brush == Brushes.LightGreen)
-                    {
-                        adornerLayer.Remove(adorners[i]);
-                        //break;
-                    }
-
-                }
-            }
-        }
-
-        //private void CheckBox2_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    CheckBox checkBox = (CheckBox)sender;
-        //    Car car = (Car)checkBox.DataContext;
-        //    var adornerLayer = AdornerLayer.GetAdornerLayer(image2);
-
-        //    //加上Adorner
-        //    OutBoxAdorner outBoxAdorner = new OutBoxAdorner(image2);
-        //    outBoxAdorner.Pen = new Pen(Brushes.LightGreen, 1);
-        //    outBoxAdorner.Rectangle = car.CarRectangle;
-        //    adornerLayer.Add(outBoxAdorner);
-        //}
-
-        //private void CheckBox2_Unchecked(object sender, RoutedEventArgs e)
-        //{
-        //    CheckBox checkBox = (CheckBox)sender;
-        //    Car carGroup = (Car)checkBox.DataContext;
-        //    var adornerLayer = AdornerLayer.GetAdornerLayer(image2);
-        //    var adorners = adornerLayer.GetAdorners(image2);
-        //    if (adorners != null)
-        //    {
-        //        for (int i = 0; i < adorners.Length; i++)
-        //        {
-        //            OutBoxAdorner oba = adorners[i] as OutBoxAdorner;
-        //            if (oba != null && oba.Rectangle.Equals(carGroup.CarRectangle) && oba.Pen.Brush == Brushes.LightGreen)
-        //            {
-        //                adornerLayer.Remove(adorners[i]);
-        //                //break;
-        //            }
-
-        //        }
-        //    }
-        //}
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Car car1 = listView1.SelectedItem as Car;
+            Car car1 = captureViewer1.listView.SelectedItem as Car;
 
             if (car1 == null)
             {
@@ -218,7 +59,7 @@ namespace Gqqnbig.TrafficVolumeCalculator
                 return;
             }
 
-            Car car2 = listView2.SelectedItem as Car;
+            Car car2 = captureViewer2.listView.SelectedItem as Car;
 
             if (car2 == null)
             {
@@ -231,6 +72,8 @@ namespace Gqqnbig.TrafficVolumeCalculator
             histSimilarityTextBox.Text += CvInvoke.cvCompareHist(car1.HistG, car2.HistG, Emgu.CV.CvEnum.HISTOGRAM_COMP_METHOD.CV_COMP_CORREL).ToString() + "\r\n";
             histSimilarityTextBox.Text += CvInvoke.cvCompareHist(car1.HistB, car2.HistB, Emgu.CV.CvEnum.HISTOGRAM_COMP_METHOD.CV_COMP_CORREL).ToString() + "\r\n";
         }
+
+
 
     }
 }
