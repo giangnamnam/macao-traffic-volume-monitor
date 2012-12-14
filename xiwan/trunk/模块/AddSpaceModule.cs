bd4d1ab20efc29b830df78a17a670786a755763a
gqqnb2005@gmail.com <gqqnb2005@gmail.com@a9e5eacc-42ef-996c-78fa-4dedd5e0f03a>
2012-12-14 05:23:34 +0000

    提取了公共模块

---
+++ xiwan/trunk/模块/AddSpaceModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Gqqnbig.TrafficVolumeMonitor.Modules
+{
+    [Serializable]
+    public class AddSpaceModule
+    {
+        public int Left { get; set; }
+        public int Right { get; set; }
+        public int Top { get; set; }
+        public int Bottom { get; set; }
+
+
+        public Image<Bgr, byte> Run(Image<Bgr, byte> image)
+        {
+            var roi = new Rectangle(new Point(Left, Top), image.Size);
+            var newImage = new Image<Bgr, byte>(image.Width + Left + Right, image.Height + Top + Bottom);
+            newImage.ROI = roi;
+
+            image.Copy(newImage, null);
+            newImage.ROI = Rectangle.Empty;
+            return newImage;
+        }
+    }
+}
---
+++ xiwan/trunk/模块/SkewTransformModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace Gqqnbig.TrafficVolumeMonitor.Modules
+{
+    [Serializable]
+    public class SkewTransformModule
+    {
+        public double Angle { get; set; }
+
+        /// <summary>
+        /// 切变
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public Image<Bgr, byte> Run(Image<Bgr, byte> image)
+        {
+            Matrix<double> mat = new Matrix<double>(2, 3);
+            mat[0, 0] = 1;
+            mat[0, 1] = -Math.Tan(Angle / 180 * Math.PI);
+            mat[0, 2] = 0;
+            mat[1, 0] = 0;
+            mat[1, 1] = 1;
+            mat[1, 2] = 0;
+
+            var warpImage = image.WarpAffine(mat, INTER.CV_INTER_LINEAR, WARP.CV_WARP_DEFAULT, new Bgr(0, 0, 0));
+
+            return Utility.AutoCrop(warpImage);
+        }
+    }
+}
