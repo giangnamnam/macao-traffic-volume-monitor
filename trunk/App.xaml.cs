25b6d54287353b8f9090783ccb3c172a52575327
gqqnb2005 <gqqnb2005@a9e5eacc-42ef-996c-78fa-4dedd5e0f03a>
2013-01-27 04:11:55 +0000

    优化异常处理逻辑

--- trunk/App.xaml.cs
+++ trunk/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Gqqnbig.TrafficVolumeMonitor.UI
 {
@@ -7,5 +8,21 @@ namespace Gqqnbig.TrafficVolumeMonitor.UI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+
+            MessageBox.Show(ex.Message + "\r\n" + ex.StackTrace);
+
+            System.Diagnostics.Debug.WriteLine(ex.Message + "\r\n" + ex.StackTrace);
+
+            Environment.Exit(1);
+        }
     }
 }
