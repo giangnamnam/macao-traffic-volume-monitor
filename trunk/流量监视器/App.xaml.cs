8b1656ccea0278ce3ed1f8d69032ca9e367d5b28
gqqnb2005@gmail.com <gqqnb2005@gmail.com@a9e5eacc-42ef-996c-78fa-4dedd5e0f03a>
2013-01-30 05:56:16 +0000

    增加流量监视器项目

---
+++ trunk/流量监视器/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace Gqqnbig.TrafficVolumeMonitor.UI
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+
+            MessageBox.Show(ex.Message + "\r\n" + ex.StackTrace);
+
+            System.Diagnostics.Debug.WriteLine(ex.Message + "\r\n" + ex.StackTrace);
+
+            Environment.Exit(1);
+        }
+    }
+}
