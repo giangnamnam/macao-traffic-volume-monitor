5412da8cd14b5d0a0cf9d9e97291539c31669396
gqqnb2005@gmail.com <gqqnb2005@gmail.com@a9e5eacc-42ef-996c-78fa-4dedd5e0f03a>
2013-02-04 05:39:30 +0000

    增加了切换语言菜单，但还不能切换。

---
+++ 流量监视器/StateFlagConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Gqqnbig.TrafficVolumeMonitor.UI
+{
+    [ValueConversion(typeof(CultureInfo), typeof(ImageSource))]
+    class StateFlagConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                CultureInfo c = (CultureInfo)value;
+
+                return new BitmapImage(new Uri("/StateFlags/" + c.Name + ".png", UriKind.Relative));
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
