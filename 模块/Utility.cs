1c794a0a0bf1f9f4ae9087067362cae72eea208e
gqqnb2005@gmail.com <gqqnb2005@gmail.com@a9e5eacc-42ef-996c-78fa-4dedd5e0f03a>
2013-04-06 02:53:28 +0000

    调整AutoCrop。

--- 模块/Utility.cs
+++ 模块/Utility.cs
@@ -4,14 +4,16 @@ using Emgu.CV.Structure;
 
 namespace Gqqnbig.TrafficVolumeMonitor.Modules
 {
-    static class Utility
+    public static class Utility
     {
-        public static Image<Bgr,byte> AutoCrop(Image<Bgr,byte> image)
+        public static Image<Bgr, byte> AutoCrop(Image<Bgr, byte> image)
         {
             var gray = image.Convert<Gray, byte>();
             CvInvoke.cvThreshold(gray, gray, 0, 255, THRESH.CV_THRESH_BINARY);
 
-            var contour= gray.FindContours();
+            var contour = gray.FindContours();
+            while (contour.BoundingRectangle.Width < 30 && contour.BoundingRectangle.Height < 30)
+                contour = contour.HNext;
             return image.Copy(contour.BoundingRectangle);
         }
     }
