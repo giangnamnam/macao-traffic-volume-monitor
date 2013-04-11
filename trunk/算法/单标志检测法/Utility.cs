2b4a2a3b69fd7c0deb5c733483765ec6234172e8
gqqnb2005@gmail.com <gqqnb2005@gmail.com@a9e5eacc-42ef-996c-78fa-4dedd5e0f03a>
2013-04-11 10:17:10 +0000

    使用模块的AutoCrop方法。

--- trunk/算法/单标志检测法/Utility.cs
+++ trunk/算法/单标志检测法/Utility.cs
@@ -8,14 +8,6 @@ namespace Gqqnbig.TrafficVolumeMonitor
 {
     static class Utility
     {
-        public static Image<Bgr,byte> AutoCrop(Image<Bgr,byte> image)
-        {
-            var gray = image.Convert<Gray, byte>();
-            CvInvoke.cvThreshold(gray, gray, 0, 255, THRESH.CV_THRESH_BINARY);
-
-            var contour= gray.FindContours();
-            return image.Copy(contour.BoundingRectangle);
-        }
 
         internal static Image<Gray, byte> FindSobelEdge(Image<Gray, byte> grayImage)
         {
